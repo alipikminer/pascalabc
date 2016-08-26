7c75c34e7ef35ace1143664dbea82349fb8a447b
alipikminer <alipikminer@yandex.ru>
2016-08-26 17:10:02 +0300

    Обновление 816.1
    Добавлен счёт версий. Добавлены новые биомы. Удалены эксперементальные
    биомы. Исправлена ошибка приводящая к вылету проведена оптимизация для
    слабых ПК. Понижены системные требования(размер монитора).

--- Program2.pas
+++ Program2.pas
@@ -1,48 +1,44 @@
 uses graphabc;
 var
-  map: array[1..900] of array[1..900] of integer;
+  map: array[1..700] of array[1..700] of integer;
 
 var
-  ii, i, x, y,xr,yr,xf,yf,xi,yi: integer;
+  ii, i, x, y,xr,yr,xf,yf,xi,yi,biome,razmer: integer;
 
 var
   c: color;
-
-procedure mapping();
-var
-  ic, iic: integer;
-begin
-  for ic := 1 to 900 do
-  begin
-    for iic := 1 to 900 do
-    begin
-      map[ic][iic] := random(0, 1)
-    end;
-  end;
-end;
 
 procedure ostrova();
 var
   i, ii: integer;
 begin
-
-    x := random(70, 850);
-    y := random(70, 850);
+biome:=random(0,7);
+razmer:=random(1,24);
+    x := random(70, 650);
+    y := random(70, 650);
     xi:=x;
     yi:=y;
-  for ii:= 1 to 100 do
+  for ii:= 1 to 250 do
   begin
-  xr:=random(-7,7);
-  yr:=random(-7,7);
-  setpixel(xi,yi,clgreen);
+  xr:=random(-razmer,razmer);
+  yr:=random(-razmer,razmer);
+  if xi<=70 then xi:=71;
+  if yi<=70 then yi:=71;
+  if xi>=650 then xi:=649;
+  if yi>=650 then yi:=649;
+  map[xi][yi]:=biome;
 
-  for i := 1 to 200 do
+  for i := 1 to 250 do
   begin    
     xr:= random(-5,5);
     yr:= random(-5,5);
-    setpixel(xf,yf,clgreen);
     xf:=xi+xr;
     yf:=yi+yr;
+    if xf<=20 then xf:=21;
+    if yf<=20 then yf:=21;
+    if xf>=650 then xf:=649;
+    if yf>=650 then yf:=649;
+    map[xf][yf]:=biome;
   end;
    xi:=xi+xr;
   yi:=yi+yr;
@@ -51,20 +47,27 @@ end;
 
 begin
   randomize();
-  mapping();
+
   sleep(1000);
-  for i := 1 to 900 do
+   for i:= 1 to 24 do
+ begin
+ ostrova();
+end;
+  for i := 1 to 700 do
   begin
-    for ii := 1 to 900 do
+    for ii := 1 to 700 do
     begin
       if map[i][ii] = 0 then c := clBlue;
-      // if map[i][ii]=1 then c:= clGreen ;
+      if map[i][ii]=1 then c:= clBlue ;
+      if map[i][ii]=2 then c:=clChocolate;
+      if map[i][ii]=3 then c:=clForestGreen;
+      if map[i][ii]=4 then c:=clGreen;
+      if map[i][ii]=5 then c:=clGreen;
+      if map[i][ii]=6 then c:=clGray;
+      if map[i][ii]=7 then c:=clOlive;
       setpixel(i, ii, c);
     end;
     
   end;
- for i:= 1 to 12 do
- begin
- ostrova();
-end;
+
 end.
