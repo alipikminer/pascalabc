bb127292bdddb8426ae4ea8274bf6eacc8c1e20a
alipikminer <alipikminer@yandex.ru>
2016-08-26 15:49:06 +0300

    Биомы, оптимизация
    оптимизировал

--- Program2.pas
+++ Program2.pas
@@ -1,6 +1,6 @@
 uses graphabc;
 var
-  map: array[1..700] of array[1..700] of integer;
+  map: array[1..900] of array[1..900] of integer;
 
 var
   ii, i, x, y,xr,yr,xf,yf,xi,yi: integer;
@@ -12,9 +12,9 @@ procedure mapping();
 var
   ic, iic: integer;
 begin
-  for ic := 1 to 700 do
+  for ic := 1 to 900 do
   begin
-    for iic := 1 to 700 do
+    for iic := 1 to 900 do
     begin
       map[ic][iic] := random(0, 1)
     end;
@@ -26,21 +26,26 @@ var
   i, ii: integer;
 begin
 
-    x := random(6, 650);
-    y := random(6, 650);
+    x := random(70, 850);
+    y := random(70, 850);
     xi:=x;
     yi:=y;
-  for ii:= 1 to 20 do
+  for ii:= 1 to 100 do
   begin
-  
+  xr:=random(-7,7);
+  yr:=random(-7,7);
+  setpixel(xi,yi,clgreen);
+
   for i := 1 to 200 do
   begin    
     xr:= random(-5,5);
     yr:= random(-5,5);
     setpixel(xf,yf,clgreen);
-    xf:=x+xr;
-    yf:=y+yr;
+    xf:=xi+xr;
+    yf:=yi+yr;
   end;
+   xi:=xi+xr;
+  yi:=yi+yr;
   end;
 end;
 
@@ -48,9 +53,9 @@ begin
   randomize();
   mapping();
   sleep(1000);
-  for i := 1 to 700 do
+  for i := 1 to 900 do
   begin
-    for ii := 1 to 700 do
+    for ii := 1 to 900 do
     begin
       if map[i][ii] = 0 then c := clBlue;
       // if map[i][ii]=1 then c:= clGreen ;
@@ -58,8 +63,8 @@ begin
     end;
     
   end;
-  for i:= 1 to 8 do
-  begin
-  ostrova();
-  end;
+ for i:= 1 to 12 do
+ begin
+ ostrova();
+end;
 end.
