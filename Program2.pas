63ac9c3311a72b246ce036c1ac4c7f90cc1ece92
alipikminer <alipikminer@yandex.ru>
2016-09-25 15:00:36 +0300

    Обновление (925.1)
    Добавлены нозвания биома

--- Program2.pas
+++ Program2.pas
@@ -1,13 +1,30 @@
 uses graphabc;
 var
   map: array[1..700] of array[1..700] of integer;
-
+  domiki: array[1..5] of array[1..5] of integer;
 var
   ii, i, x, y,xr,yr,xf,yf,xi,yi,biome,razmer: integer;
 
 var
   c: color;
+procedure derevnya();
+var i:integer;
+begin
+x := random(1,700);
+y := random(1,700);
+map[x][y]:=8;
+for i:= 1 to 2000 do
+begin
+x:= random(1,5);
+y:= random(1,5);
+if map[x][y]>=1 then
+begin
+//domiki[i][ii]:=
+map[x][y]:=8;
 
+end;
+end;
+end;
 procedure ostrova();
 var
   i, ii: integer;
@@ -22,10 +39,10 @@ razmer:=random(1,24);
   begin
   xr:=random(-razmer,razmer);
   yr:=random(-razmer,razmer);
-  if xi<=70 then xi:=71;
-  if yi<=70 then yi:=71;
-  if xi>=650 then xi:=649;
-  if yi>=650 then yi:=649;
+  if xi<=30 then xi:=31;
+  if yi<=30 then yi:=31;
+  if xi>=670 then xi:=669;
+  if yi>=670 then yi:=669;
   map[xi][yi]:=biome;
 
   for i := 1 to 250 do
@@ -36,8 +53,8 @@ razmer:=random(1,24);
     yf:=yi+yr;
     if xf<=20 then xf:=21;
     if yf<=20 then yf:=21;
-    if xf>=650 then xf:=649;
-    if yf>=650 then yf:=649;
+    if xf>=670 then xf:=669;
+    if yf>=670 then yf:=669;
     map[xf][yf]:=biome;
   end;
    xi:=xi+xr;
@@ -53,6 +70,10 @@ begin
  begin
  ostrova();
 end;
+for i:= 1 to 240 do
+begin
+derevnya();
+end;
   for i := 1 to 700 do
   begin
     for ii := 1 to 700 do
@@ -65,6 +86,7 @@ end;
       if map[i][ii]=5 then c:=clGreen;
       if map[i][ii]=6 then c:=clGray;
       if map[i][ii]=7 then c:=clOlive;
+      if map[i][ii]=8 then c:=clBlack;
       setpixel(i, ii, c);
     end;
     
